830c7c9e8b707d04ed24305cc553c9921419a7cf
shafiq.affandi <muhammad.sa@ad-ins.com>
2022-11-10 14:51:22 +0700

    add github action

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,6 @@
-import { defineConfig } from 'astro/config';
-import preact from '@astrojs/preact';
-import react from '@astrojs/react';
+import { defineConfig } from 'astro/config'
 
-// https://astro.build/config
-import netlify from "@astrojs/netlify/functions";
-
-// https://astro.build/config
 export default defineConfig({
-  integrations: [
-  // Enable Preact to support Preact JSX components.
-  preact(),
-  // Enable React for the Algolia search component.
-  react()],
-  site: `http://astro.build`,
-  output: "server",
-  adapter: netlify()
-});
+  site: 'https://shafiqaffandi.github.io',
+  base: '/astro_docu1',
+})
