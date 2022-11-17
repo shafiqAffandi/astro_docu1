a6c6fd06bf4e68120a170405e71ffd962e9c4b60
shafiq.affandi <muhammad.sa@ad-ins.com>
2022-11-17 15:57:38 +0700

    change config

--- src/config.ts
+++ src/config.ts
@@ -35,7 +35,7 @@ export const KNOWN_LANGUAGE_CODES = Object.values(KNOWN_LANGUAGES);
 export const GITHUB_EDIT_URL = `https://github.com/withastro/astro/tree/main/examples/docs`;
 
 export const COMMUNITY_INVITE_URL = `https://astro.build/chat`;
-export const BASE_URL_REPO = '/astro_docu1/';
+export const BASE_URL_REPO = 'astro_docu1/';
 
 // See "Algolia" section of the README for more information.
 export const ALGOLIA = {
